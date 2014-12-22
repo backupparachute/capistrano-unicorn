a2b60579aace4df4d506ad8da940ce36d2941fc2
kyle <github@kylemiller.com>
2014-12-22 13:21:31 -0600

    XXX added capistrano config hook.

--- lib/capistrano-unicorn.rb
+++ lib/capistrano-unicorn.rb
@@ -1,62 +1,68 @@
-set(:unicorn_binary)  { "bundle exec unicorn_rails -c #{unicorn_config} -E #{rails_env} -D" }
 
-namespace :unicorn do
-  desc "start unicorn"
-  task :start, :roles => :app, :except => { :no_release => true } do
-    puts "<><><><><><><><><><><><><>"
-    puts "STARING UNICORN..."
-    run "cd #{current_path} && #{unicorn_binary}"
-  end
-  desc "stop unicorn"
-  task :stop, :roles => :app, :except => { :no_release => true } do
-    if pid_running?(unicorn_pid)
-      run "kill `cat #{unicorn_pid}`"
+Capistrano::Configuration.instance.load do
+  # stuff here...
+
+  _cset(:unicorn_binary)  { "bundle exec unicorn_rails -c #{unicorn_config} -E #{rails_env} -D" }
+
+  namespace :unicorn do
+    desc "start unicorn"
+    task :start, :roles => :app, :except => { :no_release => true } do
+      puts "<><><><><><><><><><><><><>"
+      puts "STARING UNICORN..."
+      run "cd #{current_path} && #{unicorn_binary}"
+    end
+    desc "stop unicorn"
+    task :stop, :roles => :app, :except => { :no_release => true } do
+      if pid_running?(unicorn_pid)
+        run "kill `cat #{unicorn_pid}`"
+      end
+    end
+    desc "graceful stop unicorn"
+    task :graceful_stop, :roles => :app, :except => { :no_release => true } do
+      if pid_running?(unicorn_pid)
+        run "kill -s QUIT `cat #{unicorn_pid}`"
+      end
+    end
+    desc "graceful stop OLD unicorn"
+    task :graceful_stop_old, :roles => :app, :except => { :no_release => true } do
+      if pid_running?(unicorn_old_pid)
+        run "kill -s QUIT `cat #{unicorn_old_pid}`"
+      end
+    end
+    desc "reload unicorn"
+    task :reload, :roles => :app, :except => { :no_release => true } do
+      if pid_running?(unicorn_pid)
+        puts "UNICORN RUNNING, reloading"
+        run "kill -s USR2 `cat #{unicorn_pid}`"
+        unicorn.graceful_stop_old
+      elsif remote_file_exists?(unicorn_pid)
+        puts "REMOVING old UNICORN PID"
+        run "rm #{unicorn_pid}"
+        unicorn.start
+      else
+        unicorn.start
+      end
     end
   end
-  desc "graceful stop unicorn"
-  task :graceful_stop, :roles => :app, :except => { :no_release => true } do
-    if pid_running?(unicorn_pid)
-      run "kill -s QUIT `cat #{unicorn_pid}`"
-    end
-  end
-  desc "graceful stop OLD unicorn"
-  task :graceful_stop_old, :roles => :app, :except => { :no_release => true } do
-    if pid_running?(unicorn_old_pid)
-      run "kill -s QUIT `cat #{unicorn_old_pid}`"
-    end
-  end
-  desc "reload unicorn"
-  task :reload, :roles => :app, :except => { :no_release => true } do
-    if pid_running?(unicorn_pid)
-      puts "UNICORN RUNNING, reloading"
-      run "kill -s USR2 `cat #{unicorn_pid}`"
-      unicorn.graceful_stop_old
-    elsif remote_file_exists?(unicorn_pid)
-      puts "REMOVING old UNICORN PID"
-      run "rm #{unicorn_pid}"
-      unicorn.start
-    else
-      unicorn.start
-    end
-  end
-end
 
 
-def remote_file_exists?(full_path)
-  'true' ==  capture("if [ -e #{full_path} ]; then echo 'true'; fi").strip
-end
-
-def pid_running?(pid_file)
-  begin
-    retval = capture("ps -ef | grep `cat #{pid_file}` | grep -v grep").strip
-    puts ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-    puts "PID RUNNING: #{retval}"
-    puts ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-    return retval
-  rescue
-    puts "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
-    puts "PID DOWN..."
-    puts "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
-    return false
+  def remote_file_exists?(full_path)
+    'true' ==  capture("if [ -e #{full_path} ]; then echo 'true'; fi").strip
   end
+
+  def pid_running?(pid_file)
+    begin
+      retval = capture("ps -ef | grep `cat #{pid_file}` | grep -v grep").strip
+      puts ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+      puts "PID RUNNING: #{retval}"
+      puts ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+      return retval
+    rescue
+      puts "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+      puts "PID DOWN..."
+      puts "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+      return false
+    end
+  end
+
 end
